f08b87ec6ab6fb314085b4afd9d5a187a8eadd50
Bruno Guanabara <50642456+apocanhoto@users.noreply.github.com>
2019-07-11 18:15:38 -0400

    Update initiate.bash

--- initiate.bash
+++ initiate.bash
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+########################
+#Check if user is root #
+########################
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+
 #############################
 # .bashrc config file		#
 #############################
+
 cp ./newbashrc ~/.bashrc
 exec ~/.bashrc
 
